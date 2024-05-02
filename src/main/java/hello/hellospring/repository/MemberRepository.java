c80f87dd03139b2d830c404959f014744ba50faf
fine <finelee1119@gmail.com>
2024-05-02 17:51:45 +0900

    jpa전까지

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
+
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
